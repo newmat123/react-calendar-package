75c40752968cd8f23baab6c1b378b362f40117ac
Nicklas <nicklas9211@gmail.com>
2022-11-17 18:37:29 +0100

    basic calendar

--- package-tester/src/index.js
+++ package-tester/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Calender from './Calender';
+import Calender from './Calender.tsx';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
