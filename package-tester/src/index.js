20e846aa1a8469cde2e2bbffabd53cf5cfb6adb3
Nicklas <nicklas9211@gmail.com>
2022-11-18 15:42:33 +0100

    calendar header style

--- package-tester/src/index.js
+++ package-tester/src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Calender from './Calender.tsx';
+import Calendar from './Calendar.tsx';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Calender />
+    <Calendar />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your Calender, pass a function
+// If you want to start measuring performance in your Calendar, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
